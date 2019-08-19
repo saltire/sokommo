097ceb5a737d2874c715356bba85160798bb4cff
saltire sable <saltiresable@gmail.com>
2019-08-19 12:54:24 -0400

    Update packages, including ESLint 6; add repository; remove postinstall

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   extends: 'airbnb',
   rules: {
+    'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
     'brace-style': [2, 'stroustrup'],
     'function-paren-newline': 0,
-    'jsx-a11y/label-has-for': 0,
-    'jsx-a11y/label-has-associated-control': [2, {}],
     'jsx-quotes': [2, 'prefer-single'],
+    'no-cond-assign': [2, 'except-parens'],
     'no-console': 0,
     'no-multi-assign': 0,
     'no-nested-ternary': 0,
@@ -14,6 +14,7 @@ module.exports = {
     'operator-linebreak': [2, 'after'],
     'radix': [2, 'as-needed'],
     'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 0,
     'react/prop-types': 0,
     'strict': [2, 'global'],
   },
